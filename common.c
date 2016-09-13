764fc2d5ef9fa960310b8f46a81a66f20681a5ec
ubyyj <wdyuanyoujun@163.com>
2016-09-13 13:18:52 +0800

    fix a parameter usage error for snprint()

--- common.c
+++ common.c
@@ -97,7 +97,7 @@ void log_debug(char* msg)
 
 void mystrncpy(char* desc, const char* src, int len)
 {
-    snprintf(desc, strlen(src) + 1, "%s", src, len);
+    snprintf(desc, len, "%s", src);
 }
 
 int json_int(cJSON* root, char* item)
